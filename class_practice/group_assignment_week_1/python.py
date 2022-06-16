412e4dfdd155b122664bd572293422bc8b3c831c
Alex Valdez <xvaldez0411@gmail.com>
2022-06-16 16:44:22 -0500

    moved files out

--- class_practice/group_assignment_week_1/python.py
+++
@@ -1,8 +0,0 @@
-my_list = [
-    {"name": "Alex", "age": 31, "email": "alex@email.com"},
-    {"name": "charlie", "age": 45, "email": "charlie@email.com"},
-    {"name": "john", "age": 26, "email": "john@email.com"}
-]
-
-for i in my_list:
-    print(f"Name: {i['name']}, Age: {i['age']}, Email: {i['email']}")
